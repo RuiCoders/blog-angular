652c35ab8d9772a87736011db4a9eb162a7b2251
RuiCoders <ruyx@live.com>
2021-03-24 23:37:38 -0300

    feat(Componente):inicio sem consumo

--- blogPessoal/src/app/service/auth.service.ts
+++ blogPessoal/src/app/service/auth.service.ts
@@ -1,8 +1,10 @@
+import { environment } from './../../environments/environment.prod';
 import { UserLogin } from './../model/UserLogin';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../model/User';
+
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +22,12 @@ export class AuthService {
   cadastrar(user: User): Observable<User>{
     return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
 
+  }
+  logado(){
+    let ok: boolean = false
+    if (environment.token != ''){
+      ok = true
+    }
+    return ok
   }
 }
